435d65bae6272421364dc0feec24243ea23e0542
Anthony Morris <amorriscode@gmail.com>
2022-11-29 09:48:18 -0800

    bandaid: handle all webhook errors

--- src/index.ts
+++ src/index.ts
@@ -28,53 +28,57 @@ app.post('/helius', async (req, res) => {
   const solPrice = await getSolPrice()
 
   for (const webhook of webhooks) {
-    const nftData = webhook?.events?.nft
-    if (!nftData) {
-      break
-    }
-
-    // Very simple caching to ensure we don't double post sales
-    if (postedTransactions.has(nftData.signatue)) {
-      break
-    }
-
-    const amount = nftData.amount / LAMPORTS_PER_SOL
-    const nftAddress = nftData?.nfts?.[0]?.mint || null
-    const mintAddress = new PublicKey(nftAddress || '')
-    const nft = await metaplex?.nfts()?.findByMint({ mintAddress })
-    const nftImage = nft?.json?.image
-
-    // Build the main tweet which shares sales data
-    const tweet = [`RAAAWR 🦖\n\n${nft.json?.name} sold for ◎${amount}`]
-
-    // Add the USD price if available
-    if (solPrice) {
-      tweet.push(` ($${(amount * solPrice).toFixed(2)} USD)`)
-    }
-
-    // Add the marketplace
-    if (nftData.source) {
-      const marketplace = nftData.source
-        .split('_')
-        .map((w: string) => w[0].toUpperCase() + w.substring(1).toLowerCase())
-        .join(' ')
-      tweet.push(` on ${marketplace} `)
-    }
-
-    tweet.push(`#SeizeTheClay\n\n${HYPERSPACE_URL}/token/${nft.address}`)
-
-    // Add an image if found
-    let mediaId
-    if (nftImage) {
-      mediaId = await uploadImage(nftImage)
-    }
-
     try {
-      await postTweet({ status: tweet.join(''), mediaId })
-      postedTransactions.add(nftData.signature)
-      console.log(`Succesfully posted tweet for ${nftData.signature}`)
+      const nftData = webhook?.events?.nft
+      if (!nftData) {
+        break
+      }
+
+      // Very simple caching to ensure we don't double post sales
+      if (postedTransactions.has(nftData.signatue)) {
+        break
+      }
+
+      const amount = nftData.amount / LAMPORTS_PER_SOL
+      const nftAddress = nftData?.nfts?.[0]?.mint || null
+      const mintAddress = new PublicKey(nftAddress || '')
+      const nft = await metaplex?.nfts()?.findByMint({ mintAddress })
+      const nftImage = nft?.json?.image
+
+      // Build the main tweet which shares sales data
+      const tweet = [`RAAAWR 🦖\n\n${nft.json?.name} sold for ◎${amount}`]
+
+      // Add the USD price if available
+      if (solPrice) {
+        tweet.push(` ($${(amount * solPrice).toFixed(2)} USD)`)
+      }
+
+      // Add the marketplace
+      if (nftData.source) {
+        const marketplace = nftData.source
+          .split('_')
+          .map((w: string) => w[0].toUpperCase() + w.substring(1).toLowerCase())
+          .join(' ')
+        tweet.push(` on ${marketplace} `)
+      }
+
+      tweet.push(`#SeizeTheClay\n\n${HYPERSPACE_URL}/token/${nft.address}`)
+
+      // Add an image if found
+      let mediaId
+      if (nftImage) {
+        mediaId = await uploadImage(nftImage)
+      }
+
+      try {
+        await postTweet({ status: tweet.join(''), mediaId })
+        postedTransactions.add(nftData.signature)
+        console.log(`Succesfully posted tweet for ${nftData.signature}`)
+      } catch (error) {
+        console.error(`Failed to post tweet for ${nftData.signature}:`, error)
+      }
     } catch (error) {
-      console.error(`Failed to post tweet for ${nftData.signature}:`, error)
+      console.error('Failed to tweet webhook', error)
     }
   }
 
