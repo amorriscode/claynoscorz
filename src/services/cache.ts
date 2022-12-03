fc955e3109ceed26188e0b09e76e8eeeee544e63
Anthony Morris <amorriscode@gmail.com>
2022-12-03 11:24:59 -0800

    chore: simplify cache logic

--- src/services/cache.ts
+++ src/services/cache.ts
@@ -3,21 +3,17 @@ import Redis from 'ioredis'
 const REDIS_URL = process.env.REDIS_URL ?? null
 
 export default class Cache {
-  isRedisCache
   cache
 
   constructor() {
-    this.isRedisCache = !!REDIS_URL
     this.cache = REDIS_URL ? new Redis(REDIS_URL) : new Map()
   }
 
   async get(key: string) {
-    return this.isRedisCache ? await this.cache.get(key) : this.cache.get(key)
+    return await this.cache.get(key)
   }
 
   async set(key: string, data: string | number) {
-    return this.isRedisCache
-      ? await this.cache.set(key, data)
-      : this.cache.set(key, data)
+    return await this.cache.set(key, data)
   }
 }
